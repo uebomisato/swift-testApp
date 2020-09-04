f32c763b2e68200b906ec98c04cce7107a884006
Misato Uebo <misato.uebo@tongullman.co.jp>
2020-09-05 00:38:49 +0900

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Misato Uebo on 2020/09/05.
+//  Copyright © 2020 Misato Uebo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
