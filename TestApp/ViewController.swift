061cc0a756890895bc576d6bc882b76b5d54d47b
Misato Uebo <misato.uebo@tongullman.co.jp>
2020-09-05 10:46:35 +0900

    Add: Label,Buttonの紐付け

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numLabel: UILabel!
+
+    @IBOutlet weak var addnumButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
