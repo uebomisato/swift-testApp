676749bb1e220b9a259f9336bb8849d5755f7e4c
Misato Uebo <misato.uebo@tongullman.co.jp>
2020-09-05 11:14:56 +0900

    Add: SecondViewControllerの追加・背景色の設定

---
+++ TestApp/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  TestApp
+//
+//  Created by Misato Uebo on 2020/09/05.
+//  Copyright © 2020 Misato Uebo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.brown
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
